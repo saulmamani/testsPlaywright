cff20e7915b95ac4e56f8e1ef79ab6d30e6708d6
saul mamani <luas0_1@yahoo.es>
2022-09-01 21:16:52 -0400

    suma de productos

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -73,14 +73,6 @@ public class ExampleTest {
         }
     }
 
-    private static void addProductToCart(Page page) {
-        page.click("#add-to-cart-sauce-labs-backpack");
-        page.click("#add-to-cart-sauce-labs-bike-light");
-        page.click("#add-to-cart-sauce-labs-bolt-t-shirt");
-        page.click("#add-to-cart-sauce-labs-onesie");
-        page.click("#add-to-cart-sauce-labs-fleece-jacket");
-    }
-
     //4. Registrar los datos del cliente y mostrar el resumen de la compra
     @Test
     public void registerClient()
@@ -130,9 +122,56 @@ public class ExampleTest {
         }
     }
 
+    //6. Insertar 5 productos en el carrito, y verificar que la suma de los precios sea igual al total mostrado
+    @Test
+    public void sumProductsToCart()
+    {
+        try(Playwright playwright = Playwright.create()) {
+            Page page = login(playwright);
+
+            addProductToCart(page);
+
+            int productsInCart = Integer.parseInt(page.innerText(".shopping_cart_badge"));
+
+            page.click("a.shopping_cart_link");
+
+            //extraendo precios
+            var precios = page.locator(".inventory_item_price").allInnerTexts();
+
+            double total = 0.0;
+            for(String p:precios){
+                float precioProducto = Float.parseFloat(p.substring(1));
+                total += precioProducto;
+            }
+
+            System.out.println(((double) Math.round(total * 100) / 100));
+
+            //registrar datos del cliente
+            page.click("#checkout");
+            registerClient(page);
+            page.click("#continue");
+
+            String totalPagina = page.innerText(".summary_subtotal_label");
+            double tp = (double)Math.round(Float.parseFloat(totalPagina.substring(13)) * 100) / 100;
+            System.out.println(tp);
+
+            boolean result = ((double) Math.round(total * 100) / 100) == tp;
+
+            Assert.assertTrue(result);
+        }
+    }
+
     private static void registerClient(Page page) {
         page.type("#first-name", "Saul");
         page.type("#last-name", "Mamani");
         page.type("#postal-code", "591 7612345");
+    }
+
+    private static void addProductToCart(Page page) {
+        page.click("#add-to-cart-sauce-labs-backpack");
+        page.click("#add-to-cart-sauce-labs-bike-light");
+        page.click("#add-to-cart-sauce-labs-bolt-t-shirt");
+        page.click("#add-to-cart-sauce-labs-onesie");
+        page.click("#add-to-cart-sauce-labs-fleece-jacket");
     }
 }
